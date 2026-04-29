a60231e3c7dee67644f8c68a43281a52993aaae7
Ptashnikov7 <162282137+Ptashnikov7@users.noreply.github.com>
2026-04-29 14:19:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# Модель: Метод Ньютона (5 семестр)
+# Автор: Пташников Василь AI-235
 
+def f(x):
+    return x**2 - 2
+
+def f_prime(x):
+    return 2*x
+
+def newton_method(x0, eps=0.0001):
+    x = x0
+    while abs(f(x)) > eps:
+        x = x - f(x)/f_prime(x)
+    return x
+
+root = newton_method(1)
+print("Корінь:", root)
